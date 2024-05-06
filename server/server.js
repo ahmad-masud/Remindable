a9ec2ae7f18b2d2e6e3c2edd96a65b3c69b82c0f
Ahmad Masud <ahmadmasud25@hotmail.com>
2024-05-05 17:38:27 -0700

    update

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express()
 const reminderRoutes = require('./routes/reminders')
 const userRoutes = require('./routes/user')
 const mongoose = require('mongoose')
+const port = process.env.PORT || 3001
 
 app.use(express.json())
 app.use((req, res, next) => {
@@ -16,8 +17,8 @@ app.use('/api/users', userRoutes)
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB')
-        app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port ${process.env.PORT}`)
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`)
         })
     })
     .catch(err => {
