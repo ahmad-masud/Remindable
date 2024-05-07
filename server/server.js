eda262b2417a13314271d1fbe5bf09c9566654a8
Ahmad Masud <ahmadmasud25@hotmail.com>
2024-05-06 18:59:50 -0700

    update

--- server/server.js
+++ server/server.js
@@ -4,21 +4,24 @@ const app = express()
 const reminderRoutes = require('./routes/reminders')
 const userRoutes = require('./routes/user')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const port = 4000
 
+app.use(cors(
+    {
+        origin: 'https://remindable.vercel.app',
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+        optionsSuccessStatus: 200,
+        preflightContinue: true
+    }
+))
 app.use(express.json())
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(200);
-    }
-    next();
-});
+    console.log(req.path, req.method)
+    next()
+})
 app.use('/api/reminders', reminderRoutes)
 app.use('/api/users', userRoutes)
 
