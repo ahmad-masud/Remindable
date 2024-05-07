81316113dcd65aa3d544cf24c5c5141147f606f5
Ahmad Masud <ahmadmasud25@hotmail.com>
2024-05-06 18:51:48 -0700

    update

--- server/server.js
+++ server/server.js
@@ -4,8 +4,19 @@ const app = express()
 const reminderRoutes = require('./routes/reminders')
 const userRoutes = require('./routes/user')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const port = 4000
 
+app.use(cors(
+    {
+        origin: 'https://remindable.vercel.app',
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+        optionsSuccessStatus: 200,
+        preflightContinue: true
+    }
+))
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method)
